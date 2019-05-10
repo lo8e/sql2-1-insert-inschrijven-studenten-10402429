67dc1b6733a4ce772aab5835d0bc78c39ecd43e3
1040249 <1040249@mborijnland.nl>
2019-05-10 13:04:24 +0200

    1. Klassen aanmaken
    3 records toegevoegd

---
+++ inschrijvingen.sql
@@ -0,0 +1,3 @@
+USE lo8e_sql2;
+
+INSERT INTO klassen (klas_code,slb_code,cohort,opleiding_code) values ( "lo9e-amo1","rs001","2019","amo"),( "lo9e-amo2","bkr02","2019","amo"),( "lo9e-amo3","vlt03","2019","amo")
