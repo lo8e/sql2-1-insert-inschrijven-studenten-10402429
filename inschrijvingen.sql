74eed1f3341aec55e837360b5114d296e36c9fa0
1040249 <1040249@mborijnland.nl>
2019-05-17 12:47:41 +0200

    opdracht 1

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,3 +1,4 @@
 USE lo8e_sql2;
 
-INSERT INTO klassen (klas_code,slb_code,cohort,opleiding_code) values ( "lo9e-amo1","rs001","2019","amo"),( "lo9e-amo2","bkr02","2019","amo"),( "lo9e-amo3","vlt03","2019","amo")
+INSERT INTO studenten (ov_nummer,achternaam,tussenvoegsel,voorletters,voornaam,geboortedatum,postcode,plaats,gewicht,lengte,actief,inschrijvings_datum) 
+values ( "90001 ","varken","het","j.v","jens","20-01-2000","2394","Leiden","100","180 cm","ja","2015"),( "90002 ","koe","het","j.v","Henk","20-01-2000","2394","Leiden","100","180 cm","ja","2015"),( "90003 ","paard","het","j.v","Hans","20-01-2000","2394","Leiden","100","180 cm","ja","2015")
