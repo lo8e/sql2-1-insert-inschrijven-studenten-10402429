9ec2cf5956f28b6353dae154b8d41b55a4d3c789
1040249 <1040249@mborijnland.nl>
2019-05-17 13:12:59 +0200

    6.

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,5 +1,8 @@
 USE lo8e_sql2;
 
-UPDATE studenten
-SET inschrijvings_datum = "inactief" 
-WHERE inschrijvings_datum = '1-1-2017';
+SELECT studenten.ov_nummer, studenten.voornaam,studenten.tussenvoegsel,studenten.achternaam,klassen.klas_code,klassen_cohort,docenten.voorletters,docenten.tussenvoegsel,docenten.achternaam
+ FROM studenten
+ LEFT JOIN klassen_studenten ON klassen_studenten.ov_nummer = studenten.ov_nummer
+LEFT JOIN klassen ON klassen.klas_code = klassen_studenten.klas_code
+LEFT JOIN docenten ON docenten.docent_code = klassen.slb_code
+ ORDER BY klassen.klas_code, studenten.achternaam
