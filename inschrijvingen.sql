fce0955078dd690d324253eb46076ecde3739864
1040249 <1040249@mborijnland.nl>
2019-05-17 13:04:25 +0200

    4 wijzigen student

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,4 +1,5 @@
 USE lo8e_sql2;
 
-INSERT INTO studenten (ov_nummer,achternaam,tussenvoegsel,voorletters,voornaam,geboortedatum,postcode,plaats,gewicht,lengte,actief,inschrijvings_datum) 
-values ( "90001 ","varken","het","j.v","jens","20-01-2000","2394","Leiden","100","180 cm","ja","2015"),( "90002 ","koe","het","j.v","Henk","20-01-2000","2394","Leiden","100","180 cm","ja","2015"),( "90003 ","paard","het","j.v","Hans","20-01-2000","2394","Leiden","100","180 cm","ja","2015")
+UPDATE studenten
+SET geboortedatum = "20-01-1993" 
+WHERE ov_nummer = '90003';
